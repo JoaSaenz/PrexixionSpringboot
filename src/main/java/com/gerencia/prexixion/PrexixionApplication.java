0328135948fe6cbe9a09af7a784cba41bd2f1663
JOAQUIN TORRES <Developer03@GERENCIA.COM>
2023-10-21 12:43:22 -0500

    Primer commit

---
+++ src/main/java/com/gerencia/prexixion/PrexixionApplication.java
@@ -0,0 +1,13 @@
+package com.gerencia.prexixion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrexixionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrexixionApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/gerencia/prexixion/controller/LoginController.java
@@ -0,0 +1,5 @@
+package com.gerencia.prexixion.controller;
+
+public class LoginController {
+    
+}
